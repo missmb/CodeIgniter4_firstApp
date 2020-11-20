e97cae36de08166607f325e1552410cd9a61e6b7
Mar'atul Bariroh <maratulbariroh3630@gmail.com>
2020-11-20 08:49:02 +0700

    sendDataFromController and multipleViews

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -4,11 +4,22 @@ class Pages extends BaseController
 {
 	public function index()
 	{
-		return view('pages/home');
+		$data = [
+			'title' => 'Home | MissMb',
+			'tes' => ['one','two','tree']
+		];
+		echo view('layout/header', $data);
+		echo view('pages/home');
+		echo view('layout/footer');
 	}
 	public function about()
 	{
-		return view('pages/about');
+		$data = [
+			'title' => 'About Me'
+		];
+		echo view('layout/header', $data);
+		echo view('pages/about');
+		echo view('layout/footer');
 	}
 
 	//--------------------------------------------------------------------
